a7059d4c8debb7207907af20202635d04a0e57d3
佐罗斯 <136051963+Zokkk66@users.noreply.github.com>
2024-10-26 18:44:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,11 @@
 // script.js
 
 document.addEventListener('DOMContentLoaded', function () {
-    const text = document.getElementById('color-changing-text');
+    const title = document.getElementById('color-changing-title');
     const colors = ['red', 'blue', 'green', 'yellow', 'purple'];
     let index = 0;
     setInterval(function () {
-        text.style.color = colors[index];
+        title.style.color = colors[index];
         index = (index + 1) % colors.length;
     }, 1000);
-
-    // 模态窗口功能
-    const modal = document.getElementById('myModal');
-    const closeButton = document.querySelector('.close-button');
-    const openModalButton = document.getElementById('openModalButton');
-
-    // 打开模态窗口
-    function openModal() {
-        modal.style.display = 'block';
-    }
-
-    // 关闭模态窗口
-    function closeModal() {
-        modal.style.display = 'none';
-    }
-
-    // 点击关闭按钮关闭模态窗口
-    closeButton.addEventListener('click', closeModal);
-
-    // 点击窗口外部关闭模态窗口
-    window.addEventListener('click', function (event) {
-        if (event.target == modal) {
-            closeModal();
-        }
-    });
-
-    // 点击打开模态窗口按钮
-    openModalButton.addEventListener('click', openModal);
 });
